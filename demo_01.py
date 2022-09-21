8e6bc8dc2b8f2a11cd11b98c08c1df717f55b425
sayan <sayantankar01@gmail.com>
2022-09-21 14:35:32 +0530

    Initial commit

--- demo_01.py
+++ demo_01.py
@@ -2,6 +2,13 @@
 
 n = int(input("Enter the range: "))
 for i in range(n):
-    j=i%2
-    if j==0:
+    j = i % 2
+    if j == 0:
         print(i)
+
+# Q2. Write a program to find the factorial of  N number by taking user input.
+j = 1
+n = int(input("Enter the range: "))
+for i in range(1, n+1):
+    j = j*i
+print(j)
